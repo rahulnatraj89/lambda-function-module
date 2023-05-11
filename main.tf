767050a9af20de23caaef7da2d73e2a9b8e8e93c
rahulnatraj89 <122457791+rahulnatraj89@users.noreply.github.com>
2023-05-11 17:07:33 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,66 +1,65 @@
-resource "aws_lambda_function" "lambda" {
-  function_name = ""
-  filename = "lambda_function.zip"
-  handler = "lambda_function"
-  runtime = "python3.9"
-  publish = true
-  role = aws_iam_role.lambda.arn
+variable "region" {
+  default = "us-west-2"
 }
 
-resource "aws_lambda_function" "lambda-function" {
-  function_name = ""
-  filename = "lambda_function.zip"
-  handler = "lambda_function"
-  runtime = "python3.9"
-  publish = true
-  role = aws_iam_role.lambda.arn
+variable "aws_account" {
+  default = "123456789012"
 }
 
-resource "aws_lambda_event_source_mapping" "lambda_2" {
-  event_source_arn = "arn:aws:sqs:${var.region}:${var.aws_account}:${var.name}"
-  function_name    = aws_lambda_function..arn
+resource "aws_iam_policy_document" "lambda" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogStreams",
+    ]
+    resources = ["*"]
+  }
 }
 
-
-resource "aws_lambda_function" "lambda_3" {
-  function_name = ""
-  filename = "lambda_function.zip"
-  handler = "lambda_function"
-  runtime = "python3.9"
-  publish = true
-  role = aws_iam_role.lambda.arn
+resource "aws_iam_policy" "lambda" {
+  name = "lambda_policy"
+  policy = aws_iam_policy_document.lambda.json
 }
-resource "aws_lambda_event_source_mapping" "lambda_2" {
-  event_source_arn = "arn:aws:sqs:${var.region}:${var.aws_account}:${var.name}"
-  function_name    = aws_lambda_function.lambda_2.arn
-}
-resource "aws_iam_role_policy_attachment" "assume_role" {
-
- policy_arn = aws_iam_policy.sqs_lambda.arn
- role       = aws_iam_role.lambda.name
-}
-
-resource "aws_iam_policy" "sqs_lambda" {
-  name   = ""
-  policy = "${data.aws_iam_policy_document.this.json}"
-}
-
 
 resource "aws_iam_role" "lambda" {
-name = ""
+  name = "lambda_role"
 
-assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
-EOF
+
+resource "aws_iam_role_policy_attachment" "lambda" {
+  policy_arn = aws_iam_policy.lambda.arn
+  role       = aws_iam_role.lambda.name
+}
+
+resource "aws_lambda_function" "lambda" {
+  count = 10
+
+  function_name = "lambda_${count.index}"
+  filename = "lambda_function.zip"
+  handler = "lambda_function"
+  runtime = "python3.9"
+  publish = true
+  role = aws_iam_role.lambda.arn
+}
+
+resource "aws_lambda_event_source_mapping" "lambda" {
+  count = 10
+
+  event_source_arn = "arn:aws:${var.region}:${var.aws_account}:sqs:my-queue"
+  function_name    = aws_lambda_function.lambda[count.index].arn
 }
